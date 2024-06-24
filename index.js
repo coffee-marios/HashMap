2debe6bff147a5c7d1bce365ad5276a6d819239e
coffee-marios <mark_theo@yahoo.com>
2024-06-24 09:56:08 +0300

    Add the classes for creating a linkded list and one for the hash methods

---
+++ index.js
@@ -0,0 +1,16 @@
+class Linked_list {
+  constructor() {
+    this.head = null;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.nextValue = null;
+  }
+}
+
+class HashMap {
+  constructor() {}
+}
