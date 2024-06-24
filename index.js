b13b3ac4a54557aeb3ded9ea72943bca66b6f039
coffee-marios <mark_theo@yahoo.com>
2024-06-24 10:12:29 +0300

    Create a function hash(key) that takes a key and produces a hash code

--- index.js
+++ index.js
@@ -13,4 +13,16 @@ class Node {
 
 class HashMap {
   constructor() {}
+
+  function hash(key){
+    let hashCode = 0;
+    const PRIMENUMBER = 31;
+    const modValue = 1e9 + 9; // A large prime number
+
+    for (let index = 0; index < key.length; index++) {
+        hashCode = (PRIMENUMBER * hashCode + key.charCodeAt(index)) % modValue;
+        
+    }
+
+  }
 }
