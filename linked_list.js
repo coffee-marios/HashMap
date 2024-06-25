8c72e768d862e8dc4e31056c5acace084635a26a
coffee-marios <mark_theo@yahoo.com>
2024-06-25 09:40:39 +0300

    Store the classes for the linked list in a seperate file and add its type (module) in package.json

---
+++ linked_list.js
@@ -0,0 +1,70 @@
+class Linked_list {
+  constructor() {
+    this.head = null;
+  }
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      newNode.nextNode = this.head;
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.nextNode) {
+        current = current.nextNode;
+      }
+      current.nextNode = newNode;
+    }
+  }
+  find(key) {
+    let current = this.head;
+    let index = 0;
+    if (current === null) return null;
+
+    let myKey = Object.keys(current.value);
+
+    if (myKey[0] === key) return 0;
+
+    while (current.nextNode !== null) {
+      index += 1;
+      myKey = Object.keys(current.nextNode.value);
+      if (myKey[0] === key) return index;
+      //  if (current.nextNode.value === value) return index;
+      current = current.nextNode;
+    }
+    return null;
+  }
+  removeAt(index) {
+    if (index === 0) {
+      this.head = this.head.nextNode;
+      return;
+    }
+
+    let ind = 0;
+    let current = this.head;
+
+    while (ind < index) {
+      if (ind === index - 1) {
+        if (current.nextNode.nextNode) {
+          current.nextNode = current.nextNode.nextNode;
+        } else {
+          current.nextNode = null;
+        }
+        break;
+      } else {
+        current = current.nextNode;
+      }
+
+      ind++;
+    }
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.nextValue = null;
+  }
+}
+
+export { Linked_list };
