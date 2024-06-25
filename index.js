f8d2233e7833589ce5d2bba3b2d15e1e948ca6eb
coffee-marios <mark_theo@yahoo.com>
2024-06-25 09:31:40 +0300

    Create a function set(key, value) that registers the pair {key: value} in the hash map

--- index.js
+++ index.js
@@ -2,6 +2,62 @@ class Linked_list {
   constructor() {
     this.head = null;
   }
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      newNode.nextNode = this.head;
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.nextNode) {
+        current = current.nextNode;
+      }
+      current.nextNode = newNode;
+    }
+  }
+  find(key) {
+    let current = this.head;
+    let index = 0;
+    if (current === null) return null;
+
+    let myKey = Object.keys(current.value);
+
+    if (myKey[0] === key) return 0;
+
+    while (current.nextNode !== null) {
+      index += 1;
+      myKey = Object.keys(current.nextNode.value);
+      if (myKey[0] === key) return index;
+      //  if (current.nextNode.value === value) return index;
+      current = current.nextNode;
+    }
+    return null;
+  }
+  removeAt(index) {
+    if (index === 0) {
+      this.head = this.head.nextNode;
+      return;
+    }
+
+    let ind = 0;
+    let current = this.head;
+
+    while (ind < index) {
+      if (ind === index - 1) {
+        if (current.nextNode.nextNode) {
+          current.nextNode = current.nextNode.nextNode;
+        } else {
+          current.nextNode = null;
+        }
+        break;
+      } else {
+        current = current.nextNode;
+      }
+
+      ind++;
+    }
+  }
 }
 
 class Node {
@@ -12,17 +68,60 @@ class Node {
 }
 
 class HashMap {
-  constructor() {}
+  constructor() {
+    this.capacity = 16;
+    this.load_factor = 0.8;
+    this.entries = 0;
+    this.bucket = [];
+  }
 
-  function hash(key){
+  hash(key) {
     let hashCode = 0;
     const PRIMENUMBER = 31;
     const modValue = 1e9 + 9; // A large prime number
 
     for (let index = 0; index < key.length; index++) {
-        hashCode = (PRIMENUMBER * hashCode + key.charCodeAt(index)) % modValue;
-        
+      hashCode = (PRIMENUMBER * hashCode + key.charCodeAt(index)) % modValue;
     }
 
+    return hashCode;
+  }
+  set(key, value) {
+    let hashed_key = this.hash(key);
+    let index_bucket = hashed_key % 16;
+    console.log(index_bucket);
+
+    if (this.bucket[index_bucket] === undefined) {
+      this.bucket[index_bucket] = new Linked_list();
+      this.entries++;
+      let critical_size = this.capacity * this.load_factor;
+      console.log(["test", this.entries, critical_size]);
+      if (critical_size < this.entries) {
+        return ["We need to grow the capacity", this.entries];
+      }
+    }
+
+    let keyExists = this.bucket[index_bucket].find(key);
+    if (keyExists !== null) {
+      this.bucket[index_bucket].removeAt(keyExists);
+      // return this.bucket[index_bucket];
+    }
+    const pair = {};
+    const _key = key;
+    pair[_key] = value;
+    this.bucket[index_bucket].append(pair);
+    console.log("W: ", this.bucket[index_bucket]);
+    return true; // this.bucket;
   }
 }
+
+let test = new HashMap();
+console.clear();
+//console.log(test.hash("marios"));
+console.log(test.set("marios", "boss"));
+// console.log(test.set("marios", "09"));
+console.log(test.set("carlos", "boss"));
+console.log(test.set("carla", "09"));
+// console.log(test.set("maria", "abosgs"));
+// console.log("Bucket: ", test.bucket);
+console.log(test.entries);
